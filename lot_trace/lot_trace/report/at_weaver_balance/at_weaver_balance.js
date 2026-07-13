035db35d3501af84bc42c7d22de9a59d72b23818
Sunilmehra21 <119653765+Sunilmehra21@users.noreply.github.com>
2026-07-13 11:10:19 +0530

    Add files via upload
    Restructure: move app to repo root

---
+++ lot_trace/lot_trace/report/at_weaver_balance/at_weaver_balance.js
@@ -0,0 +1,8 @@
+frappe.query_reports["At-Weaver Balance"] = {
+    filters: [
+        {fieldname: "weaver", label: __("Weaver (Supplier)"), fieldtype: "Link",
+         options: "Supplier"},
+        {fieldname: "root_lot", label: __("Root Lot"), fieldtype: "Link",
+         options: "Root Lot"},
+    ],
+};
---
+++ lot_trace/lot_trace/report/root_lot_trace/root_lot_trace.py
@@ -0,0 +1,36 @@
+import frappe
+from frappe import _
+
+from lot_trace.api.lot import get_lot_trace
+
+
+def execute(filters=None):
+    filters = frappe._dict(filters or {})
+    if not filters.root_lot:
+        frappe.throw(_("Please select a Root Lot"))
+    data = get_lot_trace(filters.root_lot)
+    columns = [
+        {"label": _("Date"), "fieldname": "posting_date", "fieldtype": "Date", "width": 95},
+        {"label": _("Stage"), "fieldname": "stage", "fieldtype": "Link",
+         "options": "Lot Process Stage", "width": 70},
+        {"label": _("Batch"), "fieldname": "batch_no", "fieldtype": "Link",
+         "options": "Batch", "width": 170},
+        {"label": _("Item"), "fieldname": "item_code", "fieldtype": "Link",
+         "options": "Item", "width": 170},
+        {"label": _("Warehouse"), "fieldname": "warehouse", "fieldtype": "Link",
+         "options": "Warehouse", "width": 140},
+        {"label": _("Voucher Type"), "fieldname": "voucher_type", "width": 140},
+        {"label": _("Voucher"), "fieldname": "voucher_no", "fieldtype": "Dynamic Link",
+         "options": "voucher_type", "width": 160},
+        {"label": _("Qty +/-"), "fieldname": "actual_qty", "fieldtype": "Float", "width": 90},
+        {"label": _("Balance"), "fieldname": "qty_after_transaction",
+         "fieldtype": "Float", "width": 90},
+        {"label": _("UOM"), "fieldname": "stock_uom", "width": 60},
+    ]
+    return columns, data
+
+
+@frappe.whitelist()
+def get_filters():
+    return [{"fieldname": "root_lot", "label": "Root Lot", "fieldtype": "Link",
+             "options": "Root Lot", "reqd": 1}]
