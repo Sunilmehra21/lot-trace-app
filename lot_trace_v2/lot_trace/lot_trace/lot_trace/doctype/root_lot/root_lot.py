602c0fb89e2a0c782b349fd1b5785f23bdd737c2
Sunilmehra21 <119653765+Sunilmehra21@users.noreply.github.com>
2026-07-13 11:06:01 +0530

    Delete lot_trace_v2 directory

--- lot_trace_v2/lot_trace/lot_trace/lot_trace/doctype/root_lot/root_lot.py
+++
@@ -1,9 +0,0 @@
-import frappe
-from frappe.model.document import Document
-
-
-class RootLot(Document):
-    def validate(self):
-        if self.customer is None and self.sales_order:
-            self.customer = frappe.db.get_value(
-                "Sales Order", self.sales_order, "customer")
--- lot_trace_v2/lot_trace/lot_trace/lot_trace/report/order_lot_overview/order_lot_overview.js
+++
@@ -1,9 +0,0 @@
-frappe.query_reports["Order Lot Overview"] = {
-    filters: [
-        {fieldname: "sales_order", label: __("Sales Order"), fieldtype: "Link",
-         options: "Sales Order"},
-        {fieldname: "product", label: __("Product"), fieldtype: "Link", options: "Item"},
-        {fieldname: "status", label: __("Status"), fieldtype: "Select",
-         options: "\nOpen\nIn Process\nCompleted\nShort Closed"},
-    ],
-};
